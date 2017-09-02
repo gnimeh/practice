b4fdb99c09a204e09bbf97dec0639764c9d40a67
mihe <mihe@thoughtworks.com>
2017-09-03 01:10:42 +0800

    draw a horizontal line
    Signed-off-by: mihe <mihe@thoughtworks.com>

--- triangle/src/com/company/Main.java
+++ triangle/src/com/company/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        int n = 8;
+        String output = "";
+        for (int i = 0; i < 8; i++) {
+            output += "*";
+        }
+        System.out.println(output);
     }
 }
