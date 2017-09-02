00d183c04ccd0df81897b25e128099bac283dabc
mihe <mihe@thoughtworks.com>
2017-09-03 01:25:06 +0800

    draw a right triangle
    Signed-off-by: mihe <mihe@thoughtworks.com>

--- triangle/src/com/company/Main.java
+++ triangle/src/com/company/Main.java
@@ -5,8 +5,11 @@ public class Main {
     public static void main(String[] args) {
         int n = 3;
         String output = "";
-        for (int i = 0; i < 3; i++) {
-            output += "*\n";
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                output += "*";
+            }
+            output += "\n";
         }
         System.out.println(output);
     }
