cbb31d2b64820aa61b9d4d091932657fbe6bc3b8
mihe <mihe@thoughtworks.com>
2017-09-04 08:37:23 +0800

    refactory triangle and complete diamond
    Signed-off-by: mihe <mihe@thoughtworks.com>

--- triangle/src/com/company/Main.java
+++ triangle/src/com/company/Main.java
@@ -3,14 +3,48 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int n = 3;
-        String output = "";
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                output += "*";
-            }
-            output += "\n";
-        }
+        String output = diamond(3);
         System.out.println(output);
     }
+
+    private static String triangle(int n) {
+        String[][] shape = new String[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j > i) {
+                    shape[i][j] = " ";
+                } else {
+                    shape[i][j] = "*";
+                }
+            }
+            shape[i][n - 1] += "\n";
+        }
+        return shpeToString(shape);
+    }
+
+    private static String shpeToString(String[][] shape) {
+        String output = "";
+        for (int i = 0; i < shape.length; i++) {
+            for (int j = 0; j < shape[i].length; j++) {
+                output += shape[i][j];
+            }
+        }
+        return output;
+    }
+
+    private static String diamond(int n) {
+        String[][] shape = new String[n][2 * n - 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2 * n - 1; j++) {
+                if (i < Math.abs(j - n + 1)) {
+                    shape[i][j] = " ";
+                } else {
+                    shape[i][j] = "*";
+                }
+            }
+            shape[i][2 * n - 2] += "\n";
+        }
+        return shpeToString(shape);
+    }
+
 }
